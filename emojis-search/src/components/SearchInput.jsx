295877b227ce9540c437978976e63e1b51796da2
Abdou <habaieb.a@gmail.com>
2023-05-03 12:09:50 +0200

    Emojis Search

---
+++ emojis-search/src/components/SearchInput.jsx
@@ -0,0 +1,17 @@
+import { Box, TextField } from "@mui/material";
+
+export default function SearchInput({ handleChange }) {
+  function handleOnChange(e) {
+    handleChange(e.target.value);
+  }
+
+  return (
+    <Box>
+      <TextField
+        onChange={handleOnChange}
+        fullWidth
+        label="Rechercher un emoji"
+      />
+    </Box>
+  );
+}
